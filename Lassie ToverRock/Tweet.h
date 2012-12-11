262ad68ab59c99a795a1453dad6b86350c3163e6
Matthijs Hoekstra <matthijshoekstra@me.com>
2012-12-11 17:02:12 +0100

    Little update on the Twitter section

---
+++ Lassie ToverRock/Tweet.h
@@ -0,0 +1,13 @@
+//
+//  Tweet.h
+//  Lassie
+//
+//  Created by Matthijs Hoekstra on 11-12-12.
+//  Copyright (c) 2012 Richard van der Meer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tweet : NSObject
+
+@end
---
+++ Lassie ToverRock/Tweet.m
@@ -0,0 +1,13 @@
+//
+//  Tweet.m
+//  Lassie
+//
+//  Created by Matthijs Hoekstra on 11-12-12.
+//  Copyright (c) 2012 Richard van der Meer. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@implementation Tweet
+
+@end
