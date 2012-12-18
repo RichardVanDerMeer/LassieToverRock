d1333ed086a2ee30961d308a20aa836c454fcea4
Richard van der Meer <richard@richardvandermeer.nl>
2012-12-18 18:43:01 +0100

    Added FAQ

--- Lassie ToverRock/FAQViewController.h
+++ Lassie ToverRock/FAQViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface FAQViewController : UITableViewController
-
+{
+	NSMutableArray *faq;
+}
 @end
--- Lassie ToverRock/FAQViewController.m
+++ Lassie ToverRock/FAQViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "FAQViewController.h"
+#import "FAQModel.h"
+#import "FAQCell.h"
+#import "FAQDetailViewController.h"
 
 @interface FAQViewController ()
 
@@ -23,6 +26,8 @@
 		[tbi setImage:[UIImage imageNamed:@"faq.png"]];
         [tbi setTitle:@"FAQ"];
 		self.title = @"FAQ";
+		
+		faq = [[NSMutableArray alloc] init];
     }
     return self;
 }
@@ -31,11 +36,40 @@
 {
     [super viewDidLoad];
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    NSData *programmaData = [NSData dataWithContentsOfURL: [NSURL URLWithString:@"http://www.lassietoverrock.nl/app/faq.php"] options:NSDataReadingUncached error:nil];
+	
+	NSDictionary *json = nil;
+	if (programmaData) {
+		json = [NSJSONSerialization
+				JSONObjectWithData:programmaData
+				options:NSJSONWritingPrettyPrinted
+				error:nil];
+	}
+	
+	dispatch_async(dispatch_get_main_queue(), ^{
+		[self updateUIWithDictionary: json];
+	});
+}
+
+-(void)updateUIWithDictionary:(NSDictionary*)json {
+	@try {
+		for (int i = 0; i < [json[@"faq"] count]; i++) {
+			FAQModel *q = [[FAQModel alloc] init];
+			q.question = json[@"faq"][i][@"q"];
+			q.answer = json[@"faq"][i][@"a"];
+			q.URL = json[@"faq"][i][@"url"];
+			[faq addObject:q];
+		}
+		[self.tableView reloadData];
+	}
+	@catch (NSException *exception) {
+        [[[UIAlertView alloc] initWithTitle:@"Fout"
+                                    message:@"Kan het programma niet ophalen"
+                                   delegate:nil
+                          cancelButtonTitle:@"Sluiten"
+                          otherButtonTitles: nil] show];
+        NSLog(@"Exception: %@", exception);
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -48,78 +82,36 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return [faq count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    
-    // Configure the cell...
+    FAQCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+	if (!cell) {
+		cell = [[FAQCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+	}
+	
+	FAQModel *q = [faq objectAtIndex:[indexPath row]];
+	[cell setFAQ:q];
     
     return cell;
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
+    
+	FAQDetailViewController *faqViewController = [[FAQDetailViewController alloc] init];
+	[faqViewController setFAQ:[faq objectAtIndex:indexPath.item]];
+	[self.navigationController pushViewController:faqViewController animated:YES];
 }
 
 @end
